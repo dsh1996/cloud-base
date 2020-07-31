95497c349d81751b96d5985efcda3029be6347f8
dengshuihong <2765451783@qq.com>
2020-07-31 13:54:50 +0800

    服务监控

--- gateway-server/src/main/java/com/server/gatewayserver/filter/LoginTokenFilter.java
+++ gateway-server/src/main/java/com/server/gatewayserver/filter/LoginTokenFilter.java
@@ -1,13 +1,9 @@
 package com.server.gatewayserver.filter;
 
-import com.server.common.exception.AuthException;
-import com.server.common.vo.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -32,4 +28,5 @@ public class LoginTokenFilter implements GlobalFilter, Ordered {
     public int getOrder() {
         return order;
     }
+
 }
--- monitor-server/src/main/java/com/server/monitorserver/MonitorServerApplication.java
+++ monitor-server/src/main/java/com/server/monitorserver/MonitorServerApplication.java
@@ -1,5 +1,6 @@
 package com.server.monitorserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,6 +8,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @EnableTurbine
+@EnableAdminServer
 @EnableHystrixDashboard
 @EnableDiscoveryClient
 @SpringBootApplication
